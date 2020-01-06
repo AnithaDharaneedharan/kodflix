4e7216cb974e46f0bb48116ef88ecc51e15c5e08
Anitha Dharaneedharan <anitha.dharaneedharan@whitbread.com>
2020-01-06 16:35:53 +0000

    Added movie poster

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import moviePoster  from './images/popeyeMovie.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome to Kodflix
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 >Welcome to my Movie App</h1>
+     <img alt="movie_poster" src={moviePoster} />
     </div>
   );
 }
