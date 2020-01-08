e391736d8e14d65dae56001b4c061246c3c7a80f
Anitha Dharaneedharan <anitha.dharaneedharan@whitbread.com>
2020-01-08 08:46:44 +0000

    Adding 6 movie covers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import moviePoster  from './images/popeyeMovie.jpg';
-import './App.css';
+import React from "react";
+import deathNote from "./images/death-note.jpg";
+import breakingBad from "./images/breaking-bad.jpg";
+import gameOfThrones from "./images/game-of-thrones.jpg";
+import theMist from "./images/the-mist.jpg";
+import theWalkingDead from "./images/the-walking-dead.jpg";
+import theWire from "./images/the-wire.jpg";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1 >Welcome to my Movie App</h1>
-     <img alt="movie_poster" src={moviePoster} />
+      <h1>Welcome to my Movie App</h1>
+      {/* <img alt="movie_poster" src={moviePoster} />  */}
+      <div className="container">
+        <div className="item">
+          <img alt="movie_poster" src={deathNote} />
+        </div>
+        <div className="item">
+          <img alt="movie_poster" src={breakingBad} />
+        </div>
+        <div className="item">
+          <img alt="movie_poster" src={gameOfThrones} />
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <img alt="movie_poster" src={theMist} />
+        </div>
+        <div className="item">
+          <img alt="movie_poster" src={theWalkingDead} />
+        </div>
+        <div className="item">
+          <img alt="movie_poster" src={theWire} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Movies.js
@@ -0,0 +1,11 @@
+let movies = ['Black Mirror', 'Breaking Bad', 'Death Note', 'Game of Thrones', 'The Walking Dead', 'The Wire'];
+let movieList = [];
+
+function Movies() {
+    for (let movie of movies) {
+        movieList.push(movie);
+      }
+      
+    return movieList;
+}
+
