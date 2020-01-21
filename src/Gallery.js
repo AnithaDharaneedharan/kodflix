18f20112ddfb24caa0602276226424d7bffd9da1
Anitha Dharaneedharan <anitha.dharaneedharan@whitbread.com>
2020-01-21 14:09:34 +0000

    Move the cover data model to front end data source

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Movies from "./Movies.js";
-import deathNote from "./images/death-note.jpg";
-import breakingBad from "./images/breaking-bad.jpg";
-import gameOfThrones from "./images/game-of-thrones.jpg";
-import theMist from "./images/the-mist.jpg";
-import theWalkingDead from "./images/the-walking-dead.jpg";
-import theWire from "./images/the-wire.jpg";
+import getGallery from "./getGallery";
 
 export default function Gallery() {
   return (
     <div>
       <div className="container">
-        <Movies name="DeathNote" logo={deathNote} id="deathNote"></Movies>
-        <Movies name="breakingBad" logo={breakingBad} id="breakingBad"></Movies>
-        <Movies name="gameOfThrones" logo={gameOfThrones} id="gameOfThrones"></Movies>
-     
-        <Movies name="theMist" logo={theMist} id="theMist"></Movies>
-        <Movies name="theWalkingDead" logo={theWalkingDead} id="theWalkingDead"></Movies>
-        <Movies name="theWire" logo={theWire} id="theWire"></Movies>
+     {
+       getGallery().map(cover => {
+         return (
+          <Movies key={cover.id} name={cover.title} logo={cover.image} id={cover.id}/>
+         )
+       })
+
+     }
       </div>
     </div>
   );
 }
+
+
