71a38b1e8fc6acafc0499d81b34de3676ec2bae1
Anitha Dharaneedharan <anitha.dharaneedharan@whitbread.com>
2020-01-21 17:00:10 +0000

    Changing the state of the component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Details() {
-  return (
-    <div>
-    <div>Hello , this will be the details page for each Movie and TV show</div>
-    <Link to='/'>Back to homepage</Link>
-    </div>
-  );
+export default class Details extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message:
+        "Hello , this will be the details page for each Movie and TV show"
+    };
+  }
+
+  componentDidMount = () => {
+    // setTimeout( function () { return this.setState({message: "Coming soon! :)"})} , 3000)
+    setTimeout(() => {
+      this.setState({ message: "Coming soon! :)" });
+    }, 3000);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.message}</div>
+        <Link to="/">Back to homepage</Link>
+      </div>
+    );
+  }
 }
